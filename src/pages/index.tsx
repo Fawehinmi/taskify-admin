084d6f671bef94a5189dcffe8f642c1e0971c2ef
Emmanuel Fawehinmi <emmanuel@asynctechs.com>
2022-09-03 09:16:54 -0700

    updating Tailwind and type script config

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-red-800">
+      <h1 className="text-white">hello</h1>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/modules/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js, ts, jsx, tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
